bda05cc6bfefeb553ee7bf3bdd056ae88a98597a
James Shipman <jbshipman@icloud.com>
2020-01-05 15:57:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,13 @@ def directors_totals(nds)
   dir_index = 0 
   totals = {} 
   
+  # Outer loop for name of director
   while dir_index < nds.length do 
     nameDirector = nds[dir_index][:name]
     totals[nameDirector] = 0
     indexMovies = 0 
     
+    # Inner loop for movies per director 
     while indexMovies < nds[dir_index][:movies].length do 
       totals[nameDirector] += nds[dir_index][:movies][indexMovies][:worldwide_gross]
       indexMovies += 1 
